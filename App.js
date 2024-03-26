ce697b83746d534dabcb6b08563eb95e385a6dee
Ikram <ikramaslam1020@gmail.com>
2024-03-27 02:17:00 +0500

    configuring the authentication with clerk

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './Apps/Screens/LoginScreen';
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white">
-      <StatusBar style="auto" />
-      <LoginScreen />
-    </View>
+    <ClerkProvider publishableKey='pk_test_c2F2ZWQtcmF0LTU2LmNsZXJrLmFjY291bnRzLmRldiQ'>
+      <View className="flex-1 bg-white">
+        <StatusBar style="auto" />
+        <SignedIn>
+          <Text className='my-20 text-center text-red-600 bg-slate-500 text-lg font-bold p-4'>You are SignedIn</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+
+      </View>
+    </ClerkProvider>
   );
 }
 
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,7 +1,29 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View>
       <Image
@@ -13,7 +35,10 @@ export default function LoginScreen() {
         <Text className="text-[18px] text-slate-400 mt-3">
           Buy Sell Marketplace where You can Sell Old Items
         </Text>
-        <TouchableOpacity className="bg-blue-500 rounded-full p-4 mt-20">
+        <TouchableOpacity
+          onPress={onPress}
+          className="bg-blue-500 rounded-full p-4 mt-20"
+        >
           <Text className="text-center text-[18px] text-white">
             Get Started
           </Text>
