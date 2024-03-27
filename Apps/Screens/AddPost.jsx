044dd0b2c0a2ed9b504484d86cab4c02dc1e158d
Ikram <ikramaslam1020@gmail.com>
2024-03-28 02:37:29 +0500

    FireBase Setup to get completed

--- Apps/Screens/AddPost.jsx
+++ Apps/Screens/AddPost.jsx
@@ -1,7 +1,21 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { app } from "../../firebaseConfig";
 
 export default function AddPost() {
+  const db = getFirestore(app);
+  const [categoryList, setCategoryList] = useState([]);
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+  const getCategoryList = async () => {
+    const querySnapshot = await getDocs(collection(db, "Category"));
+    querySnapshot.forEach((doc) => {
+      console.log("Docs:", doc.data());
+      setCategoryList((categoryList) => [...categoryList, doc.data()]);
+    });
+  };
   return (
     <View>
       <Text>AddPost</Text>
---
+++ firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpUab_IAzo9_S8OBVZzZzKfFJrzQ_bQSw",
+  authDomain: "snap-buy-e3e4a.firebaseapp.com",
+  projectId: "snap-buy-e3e4a",
+  storageBucket: "snap-buy-e3e4a.appspot.com",
+  messagingSenderId: "114098333208",
+  appId: "1:114098333208:web:0f427d6b546938ecb5c56d",
+  measurementId: "G-YGYBBMKV1H",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
