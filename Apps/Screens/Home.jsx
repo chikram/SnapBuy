3de5a9f270d858511409978eadc93c9cb8413f5d
Ikram <ikramaslam1020@gmail.com>
2024-04-01 02:33:34 +0500

    Adding category list to the homescreen

--- Apps/Screens/Home.jsx
+++ Apps/Screens/Home.jsx
@@ -4,12 +4,15 @@ import Header from "../components/HomeScreen/Header";
 import Slider from "../components/HomeScreen/Slider";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { app } from "../../firebaseConfig";
+import Categories from "../components/HomeScreen/Categories";
 
 export default function Home() {
   const db = getFirestore(app);
   const [sliderList, setSliderList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
   useEffect(() => {
     getSlider();
+    getCategoryList();
   }, []);
   //use to get silder data
   const getSlider = async () => {
@@ -19,10 +22,21 @@ export default function Home() {
       setSliderList((sliderList) => [...sliderList, doc.data()]);
     });
   };
+  //use to get category from firebase
+  const getCategoryList = async () => {
+    setCategoryList([]);
+    const querySnapshot = await getDocs(collection(db, "Category"));
+    querySnapshot.forEach((doc) => {
+      console.log("Docs:", doc.data());
+      setCategoryList((categoryList) => [...categoryList, doc.data()]);
+    });
+  };
+
   return (
     <View className="py-8 px-2 bg-white flex-1">
       <Header />
       <Slider sliderList={sliderList} />
+      <Categories categoryList={categoryList} />
     </View>
   );
 }
--- Apps/components/HomeScreen/Categories.jsx
+++ Apps/components/HomeScreen/Categories.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
-export default function Categories() {
+export default function Categories({ categoryList }) {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View className="mt-3">
+      <Text className="font-bold text-[20px]">Categories</Text>
+      <FlatList
+        data={categoryList}
+        numColumns={4}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            className="flex-1 items-center 
+          justify-center p-2 border-[1px] border-blue-200
+          bg-blue-50  m-1 h-[80px] rounded-lg"
+          >
+            <Image source={{ uri: item?.icon }} className="h-[40px] w-[40px]" />
+            <Text className="text-[12px] font-bold mt-1">{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
